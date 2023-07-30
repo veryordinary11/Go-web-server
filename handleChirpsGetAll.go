ef5dc88f428ce2cbb11792f3235c8492233fc681
veryordinary11 <bessingeralexisi@gmail.com>
2023-07-30 13:11:56 +0800

    update get endpoint

--- handleChirpsGetAll.go
+++ handleChirpsGetAll.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/veryordinary11/Go-web-server/database"
 )
 
 func handlerChirpsGetAll(db *database.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// Grab authorId from URL
+		authorId := r.URL.Query().Get("author_id")
 		// Get all chirps from the database
 		chirps, err := db.GetChirps()
 		if err != nil {
@@ -15,7 +18,26 @@ func handlerChirpsGetAll(db *database.DB) http.HandlerFunc {
 			return
 		}
 
+		// Filter chirps by authorId
+		if authorId == "" {
+			responseWithJSON(w, http.StatusOK, chirps)
+			return
+		}
+
+		filteredChirps := []database.Chirp{}
+		authorIdInt, err := strconv.Atoi(authorId)
+		if err != nil {
+			responseWithError(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		for _, chirp := range chirps {
+			if chirp.AuthorID == authorIdInt {
+				filteredChirps = append(filteredChirps, chirp)
+			}
+		}
+
 		// Respond with the chirps
-		responseWithJSON(w, http.StatusOK, chirps)
+		responseWithJSON(w, http.StatusOK, filteredChirps)
 	}
 }
